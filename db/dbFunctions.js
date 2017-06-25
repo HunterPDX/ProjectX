ec90f4bc71bb75f1d86d5246276146ca3f10ec62
Hunter <dustin@hunterpdx.com>
2017-06-24 17:47:38 -0700

    Updates

---
+++ db/dbFunctions.js
@@ -0,0 +1,45 @@
+var knex 	= require('./db')
+var mysql   = require('mysql');
+
+function DBFunctions(table){
+	this.table = table;
+
+	this.find = function(req, callback){
+		if(typeof req != 'object' && typeof req === 'number'){		
+			knex(this.table).where({id:req}).then(function(data){
+				callback(data)
+			})
+		}else if( typeof req == 'string'){
+			if(req ==='all'){
+				knex(this.table).then(function(data){
+					callback(data)
+				})
+			}
+		}else{
+			return {
+				where: function (req, callback){
+					knex(table).where(req).then(function(data){
+						callback(data)
+					})
+				},
+				whereJoin: function (req, dataJoin, callback){
+					knex(table).where(req).then(function(data){
+						dataJoin.joined = data
+						callback(dataJoin)
+					})
+				},
+				option: function(req){
+					return req+req
+				}
+			}
+		}
+	}
+	this.create = function(req, callback){
+		req.created_at = new Date()
+		req.updated_at = new Date()
+		knex(table).insert(req).then(function(data){
+			callback(null, data)
+		})
+	}
+}
+module.exports = DBFunctions
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var dbFunctions = require('../db/dbFunctions');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/subjects', function(req, res, next) {
+  var newFunction = new dbFunctions("subjects");
+  console.log(newFunction.find(1));
 });
 
 module.exports = router;
